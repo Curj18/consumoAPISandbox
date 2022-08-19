e1abd82e1864fcda41a3bfcad320f2910765a395
jorge <jorgecurilao@gmail.com>
2022-08-18 22:34:31 -0400

    agrego consumo de Top System

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ import conf
 
 
 
-sandbox = "https://sandboxapicdc.cisco.com"
+sandbox = "https://10.10.20.14"
 
 def obtener_token(usuario, clave):
     url = "https://10.10.20.14/api/aaaLogin.json"
@@ -26,6 +26,22 @@ def obtener_token(usuario, clave):
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
 
-token = obtener_token(conf.usuario, conf.clave)
+#http://apic-ip-address/api/class/topSystem.json
+def top_system():
+    cabecera = {
+        "Content-Type" : "application/json"
+    }
+    galleta = {
+        "APIC-Cookie":  obtener_token(conf.usuario, conf.clave)
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta= requests.get(sandbox+"/api/class/topSystem.json", headers= cabecera, cookies= galleta, verify=False)
 
-print(token)
+    total_nodos =respuesta.json()["totalCount"]
+
+    for i in range(0, int(respuesta.json()["totalCount"])):  # o poner 3 en ves d elo ultimo de totalcount
+        ip_local = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["address"]
+        print(ip_local)
+
+top_system()
+
