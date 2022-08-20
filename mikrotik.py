6b14a25ee20bb30934628b3b2d9661171f7e2fdd
jorge <jorgecurilao@gmail.com>
2022-08-20 18:18:49 -0400

    api mikrootik 3

--- mikrotik.py
+++ mikrotik.py
@@ -9,8 +9,11 @@ user = "admin"
 
 clave = "admin"
 
-r = requests.get(url+"/rest/ip/address", auth= (user,clave), verify = False)
+r = requests.get(url+"/rest/ip/address/*3", auth= (user,clave), verify = False)
 
 
-print(json.dumps(r.json(), indent =2))
+#print(json.dumps(r.json(), indent =2))
 
+r2 = requests.get(url + "/rest/interface/ether1", auth= (user,clave), verify = False)
+
+print(r2.json()["mac-address"])
