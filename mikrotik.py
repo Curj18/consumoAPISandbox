e69f7eb44d591e3f74bfd7820eaae4933034d9da
jorge <jorgecurilao@gmail.com>
2022-08-20 17:59:18 -0400

    api mikrootik 1

---
+++ mikrotik.py
@@ -0,0 +1,15 @@
+import requests
+import urllib3
+import json
+
+urllib3.disable_warnings()
+
+url= "https://192.168.0.23"
+user = "admin"
+
+clave = "admin"
+
+r = requests.get(url+"/rest/ip/address", auth= (user,clave), verify = False)
+
+
+print(json.dumps(r.json(), indent =2))
